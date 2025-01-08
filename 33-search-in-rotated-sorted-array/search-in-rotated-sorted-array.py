c968023c1db87469b7f60078221019f90034479d
Ahmed Ibrahim <ahmedm3377@yahoo.com>
2025-01-07 20:53:26 -0600

    Time: 3 ms (5.06%) | Memory: 18 MB (32.15%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -2,5 +2,14 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if(target not in nums):
             return -1
+        p1 = 0
+        p2 = nums.index(min(nums))
+
+        if target>nums[-1]:
+            pass
         else:
-            return nums.index(target)
+            p1 = p2
+            p2 = len(nums)
+        for i in range(p1,p2+1):
+            if target==nums[i]:
+                return i
