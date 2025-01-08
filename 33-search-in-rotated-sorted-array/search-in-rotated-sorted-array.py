966d37c6984f45ff9202580e223b48c8bd41bf2f
Ahmed Ibrahim <ahmedm3377@yahoo.com>
2025-01-07 22:19:40 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (14.40%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,15 +1,45 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if(target not in nums):
+        if not nums:
             return -1
-        p1 = 0
-        p2 = nums.index(min(nums))
+        
+        def minIndex(i, j):
+            left, right = 0, len(nums) - 1
+
+            while left < right:
+                mid = (left + right) // 2
+
+                # Compare mid element with the rightmost element
+                if nums[mid] > nums[right]:
+                    left = mid + 1  # Minimum must be in the right half
+                else:
+                    right = mid  # Minimum is in the left half (including mid)
+
+            # When left == right, we've found the minimum
+            return left
+
+
+
+
+        def helper(i, j):
+            if i > j:
+                return -1
+
+            mid = (i + j) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if target < nums[mid]:
+                return helper(i, mid-1 )
+            if target > nums[mid]:
+                return helper(mid+1 , j)
+
+        mn = minIndex(0, len(nums) - 1)
 
         if target>nums[-1]:
-            pass
+            return helper(0, mn-1)
+
         else:
-            p1 = p2
-            p2 = len(nums)
-        for i in range(p1,p2+1):
-            if target==nums[i]:
-                return i
+            return helper(mn, len(nums)-1)
+
