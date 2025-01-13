187ba0cfb202b6a9a6966c0970b844069f40b45f
Ahmed Ibrahim <ahmedm3377@yahoo.com>
2025-01-12 21:19:26 -0600

    Time: 35 ms (34.05%) | Memory: 23.3 MB (43.52%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -5,12 +5,11 @@ class Solution:
         answer = 0
 
         for i in range(len(gas)):
-            # gain[i] = gas[i] - cost[i]
+
             total_gain += gas[i] - cost[i]
             curr_gain += gas[i] - cost[i]
 
-            # If we meet a "valley", start over from the next station
-            # with 0 initial gas.
+
             if curr_gain < 0:
                 curr_gain = 0
                 answer = i + 1
